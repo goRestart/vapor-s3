77894f8cccc3976346971401007a0cbde89a45e3
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-12 21:17:33 +0100

    Update project to use swift 4 🚀

--- Package.swift
+++ Package.swift
@@ -1,11 +1,20 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0.0
 
 import PackageDescription
 
 let package = Package(
-    name: "S3",
-    dependencies: [
-    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-    	.Package(url: "https://github.com/vzsg/S3SignerAWS.git", majorVersion: 2)
-    ]
+  name: "S3",
+  products: [
+    .library(name: "S3", targets: ["S3"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.3.0")),
+    .package(url: "https://github.com/JustinM1/S3SignerAWS.git", .upToNextMajor(from: "3.0.2"))
+  ],
+  targets: [
+    .target(
+      name: "S3",
+      dependencies: ["Vapor", "S3SignerAWS"]
+     )
+  ]
 )
